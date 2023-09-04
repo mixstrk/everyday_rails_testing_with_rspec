7a4e6b254f49452f13340ac1a096386926d23a46
Maksim <i@maxstrk.ru>
2023-09-04 15:36:01 +0300

    Basic controller specs

--- spec/factories/contacts.rb
+++ spec/factories/contacts.rb
@@ -6,9 +6,13 @@ FactoryBot.define do
 
     after(:build) do |contact|
       [:home_phone, :work_phone, :mobile_phone].each do |phone|
-        contact.phones << FactoryBot.build(:phone,
+        contact.phones << build(:phone,
           phone_type: phone, contact: contact)
       end
+    end
+
+    factory :invalid_contact do
+      firstname nil
     end
   end
 end
