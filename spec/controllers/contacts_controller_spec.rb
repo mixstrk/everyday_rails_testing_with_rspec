ed976c6fb29b1cb0af20f6f941da55ded5ca3e7b
Maksim <i@maxstrk.ru>
2023-09-08 14:05:59 +0300

    Speeding up specs

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -1,6 +1,22 @@
 require "rails_helper"
 
 describe ContactsController do
+  let(:admin) { build_stubbed(:admin) }
+  let(:user) { build_stubbed(:user) }
+  
+  let(:contact) { create(:contact, firstname: 'Max', lastname: 'Stark') }
+
+  let(:phones) do
+    [
+      attributes_for(:phone, phone_type: "home"),
+      attributes_for(:phone, phone_type: "office"),
+      attributes_for(:phone, phone_type: "mobile")
+    ]
+  end
+
+  let(:valid_attributes) { attributes_for(:contact) }
+  let(:invalid_attributes) { attributes_for(:invalid_contact) }
+
   shared_examples "public access to contacts" do
     describe "GET #index" do
       context "with params[:letter]" do
@@ -35,24 +51,27 @@ describe ContactsController do
     end
 
     describe "GET #show" do
+      let(:contact) { build_stubbed(:contact,
+        firstname: 'Max', lastname: 'Stark') }
+
+        before :each do
+          allow(Contact).to receive(:persisted?).and_return(true)
+          allow(Contact).to \
+            receive(:order).with('lastname, firstname').and_return([contact])
+          allow(Contact).to \
+            receive(:find).with(contact.id.to_s).and_return(contact)
+          allow(Contact).to receive(:save).and_return(true)
+  
+          get :show, params: { id: contact }
+        end
+        
       it "assigns the requested contact to @contact" do
-        contact = create(:contact)
-        get :show, params: { id: contact }
         expect(assigns(:contact)).to eq contact
       end
 
-      it "renders the :show template" do
-        contact = create(:contact)
-        get :show, params: { id: contact }
+      it "renders the :show template", focus: true do
         expect(response).to have_http_status(:ok)
           .and render_template :show
-      end
-
-      it "renders the :show template for the phone" do
-        contact = create(:contact)
-        phone = create(:phone, contact: contact)
-        get :show, params: { id: phone, contact_id: contact.id }
-        expect(response).to render_template :show
       end
     end
   end
@@ -91,13 +110,13 @@ describe ContactsController do
         it "saves the new contact in the database" do
           expect{
             post :create, params: { contact: attributes_for(:contact,
-              phones_attributes: @phones) }
+              phones_attributes: phones) }
           }.to change(Contact, :count).by(1)
         end
         
         it "redirects to contacts#show" do
           post :create, params: { contact: attributes_for(:contact,
-            phones_attributes: @phones) }
+            phones_attributes: phones) }
           expect(response).to redirect_to contact_path(assigns[:contact])
         end
       end
@@ -120,49 +139,43 @@ describe ContactsController do
 
     describe "PATCH #update" do
 
-      before :each do
-        @contact = create(:contact,
-          firstname: "Egor",
-          lastname: "Begov")
-      end
-
       context "with valid attributes" do
         it "locates the requested @contact" do
-          patch :update, params: { id: @contact,
+          patch :update, params: { id: contact,
             contact: attributes_for(:contact) }
-          expect(assigns(:contact)).to eq(@contact)
+          expect(assigns(:contact)).to eq(contact)
         end
 
         it "changes @contact's attributes" do
-          patch :update, params: { id: @contact,
+          patch :update, params: { id: contact,
             contact: attributes_for(:contact,
-              firstname: "George",
-              lastname: "Beglov") }
-          @contact.reload
-          expect(@contact.firstname).to eq("George")
-          expect(@contact.lastname).to eq("Beglov")
+              firstname: "Maxim",
+              lastname: "Starkov") }
+          contact.reload
+          expect(contact.firstname).to eq("Maxim")
+          expect(contact.lastname).to eq("Starkov")
         end
         
         it "redirects to the updated contact" do
-          patch :update, params: { id: @contact,
+          patch :update, params: { id: contact,
             contact: attributes_for(:contact) }
-          expect(response).to redirect_to @contact
+          expect(response).to redirect_to contact
         end
       end
 
       context "with invalid attributes" do
         it "does not change the contact's attributes" do
-          patch :update, params: { id: @contact,
+          patch :update, params: { id: contact,
             contact: attributes_for(:contact,
-              firstname: "George",
+              firstname: "Maxim",
               lastname: nil) }
-          @contact.reload
-          expect(@contact.firstname).to_not eq("George")
-          expect(@contact.lastname).to eq("Begov")
+          contact.reload
+          expect(contact.firstname).to_not eq("Maxim")
+          expect(contact.lastname).to eq("Stark")
         end
 
         it "re-renders the :edit template" do
-          patch :update, params: { id: @contact,
+          patch :update, params: { id: contact,
             contact: attributes_for(:invalid_contact) }
           expect(response).to render_template :edit
         end
@@ -170,18 +183,16 @@ describe ContactsController do
     end
 
     describe "DELETE #destroy" do
-      before :each do
-        @contact = create(:contact)
-      end
       
       it "deletes the contact from the database" do
+        contact
         expect{
-          delete :destroy, params: { id: @contact }
+          delete :destroy, params: { id: contact }
         }.to change(Contact, :count).by(-1)
       end
 
       it "redirects to users#index" do
-        delete :destroy, params: { id: @contact }
+        delete :destroy, params: { id: contact }
         expect(response).to redirect_to contacts_url
       end
     end
@@ -189,7 +200,7 @@ describe ContactsController do
   
   describe "administrator access" do 
     before :each do
-      set_user_session create(:admin)
+      allow(controller).to receive(:current_user).and_return(admin)
     end
 
     it_behaves_like "public access to contacts"
